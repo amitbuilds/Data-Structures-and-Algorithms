a472777c09d56e69dcf4f9331a2d460a6a770eca
Amit Kumar Gupta <karl097jacobs@gmail.com>
2026-01-22 17:35:42 +0530

    Time: 0 ms (100%), Space: 7.7 MB (95.73%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // if(n==0){
+        //     return 0;
+        // }
+        // if(n>0 && n<4){
+        //     return 1;
+        // }
+        // return 2;
+        return sqrt(n);
+    }
+};
