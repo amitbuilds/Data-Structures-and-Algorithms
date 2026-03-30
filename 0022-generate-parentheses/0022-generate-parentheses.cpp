bbf37889905c6f8fb6213046d7e5cb6fdfe0e5e2
Amit Kumar Gupta <karl097jacobs@gmail.com>
2026-03-30 20:52:04 +0530

    Time: 5 ms (16.85%), Space: 15.7 MB (35.84%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+private:
+    void backtrack(vector<string>& result, string current, int open, int close, int n) {
+        // If the current string is complete
+        if (current.size() == 2 * n) {
+            result.push_back(current);
+            return;
+        }
+
+        // Add "(" if we still have left
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        // Add ")" if valid
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+};
+
+
+
+
