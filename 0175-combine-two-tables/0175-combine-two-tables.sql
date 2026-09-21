4c358f0990e6d3c94e3e2f55c54387f7f98350c1
Amit Kumar Gupta <karl097jacobs@gmail.com>
2026-09-21 11:40:09 +0530

    Time: 238 ms (90.81%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,11 +1,4 @@
-# Write your MySQL query statement below
-SELECT 
-    p.firstName,
-    p.lastName,
-    a.city,
-    a.state
-FROM Person p
-LEFT JOIN Address a
-ON p.personId = a.personId;
-
-122
+-- Write your PostgreSQL query statement below
+select p.firstName,p.lastName,a.city,a.state from
+Person p left join Address a on
+p.personId = a.personId ;
